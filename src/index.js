74c9a34df9745d2667804975c8f50b678846fd6c
Juan Carlos Picado <juanpicado19@gmail.com>
2016-09-30 21:55:25 +0200

    fixed lint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
-import "babel-polyfill";
-
-import fs from 'fs';
-import path from 'path';
+import 'babel-polyfill';
 
 /**
- * Metalsmith plugin to render Youtube Video
- * @param {Object} options (optional)
- * @return {Function}
+ * Youtube iframe generator
  */
-
-function plugin( _options ) {
-  let options = _options || {};
-  return ( files, metalsmith, done ) => {
-    let metadata      = metalsmith.metadata();
-    let collections   = metadata.collections;
-
-    let colName, file, filePath;
-    let app = new Youtube( options, files );
-    app.parse( function() {
-      done();
-    });
-  };
-};
-
 class Youtube {
   constructor(options, files) {
     this.files = files;
@@ -37,26 +17,43 @@ class Youtube {
     Object.assign(this.options, options);
   }
 
+  /**
+   * Parse the content
+   * @param cb callback {Function}
+   */
   parse(cb) {
-    var self = this;
     Object.keys(this.files).forEach((file) => {
-       let article_text = this.files[file].contents.toString();
-       let matches = this._getYoutubes(article_text);
+       let articleText = this.files[file].contents.toString();
+       let matches = this._getYoutubes(articleText);
        if (matches !== null) {
-         matches.forEach((element, index) => {
+         matches.forEach((element) => {
             var id = element.split('|')[1];
             var iframe = this._createIframeBody(id, this.options.width, this.options.height);
-            this.files[file].contents = article_text.replace(element, iframe);
+            this.files[file].contents = articleText.replace(element, iframe);
          });
        }
     });
     cb();
   }
 
+  /**
+   * Return all youtube definitions within the body content
+   * @param content
+   * @returns {Array|{index: number, input: string}|*}
+   * @private
+   */
   _getYoutubes(content) {
     return content.match(/youtube\|.\S*/g);
   }
 
+  /**
+   * Create a YouTube iframe
+   * @param id the YouTube id
+   * @param width iframe width
+   * @param height iframe height
+   * @returns {string} iframe as string
+   * @private
+   */
   _createIframeBody(id, width, height) {
     let url;
     if (this.options.privacy) {
@@ -67,10 +64,26 @@ class Youtube {
     url += this.options.suggested ? `?rel=0&amp;` : `?`;
     url += this.options.showTitle ? `&amp;showinfo=0` : ``;
     url += this.options.controls ? `&amp;controls=0` : ``;
-    const iframe = `<iframe width="${width}" height="${height}" src="${url}" frameborder="0" allowfullscreen></iframe>`;
+    const iframe = `<iframe width='${width}' height='${height}' src='${url}' frameborder='0' allowfullscreen></iframe>`;
     return iframe;
   }
 }
+
+/**
+ * Metalsmith plugin to render Youtube Video
+ * @param {Object} options (optional)
+ * @return {Function}
+ */
+function plugin( _options ) {
+  let options = _options || {};
+  return ( files, metalsmith, done ) => {
+    let app = new Youtube( options, files );
+    app.parse( function() {
+      done();
+    });
+  };
+}
+
 
 /**
 * Expose `plugin`.
